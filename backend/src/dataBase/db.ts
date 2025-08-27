c44b7796aa6e326bd3c7c3e3d9c330a9dba68b71
Kirill <prostoskvasil@gmail.com>
2025-08-28 01:50:14 +0500

    fix4

--- backend/src/dataBase/db.ts
+++ backend/src/dataBase/db.ts
@@ -1,14 +1,13 @@
-import {Pool} from 'pg'
+import { Pool } from 'pg'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: Number(process.env.DB_PORT),
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === 'production' ? { 
+        rejectUnauthorized: false 
+    } : false,
     max: 20,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000
@@ -19,15 +18,13 @@ export async function testConnetion() {
     try {
         client = await pool.connect()
         console.log('PostgreSQL доступна')
-    }catch (err) {
+    } catch (err) {
         console.error('Ошибка подключения к PostgreSQL:', err)
         process.exit(1)
-    }finally {
-        if(client) client.release()
+    } finally {
+        if (client) client.release()
     }
 }
-
-testConnetion()
 
 pool.on('connect', () => console.log('Новое подключение к PostgreSQL'))
 pool.on('error', (err) => console.log('Ошибка подключения', err))
