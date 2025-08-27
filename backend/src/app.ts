0cf0873dfe29bc8901b15c18c1022a5c02b1de79
Kirill <prostoskvasil@gmail.com>
2025-08-28 02:33:23 +0500

    fixCORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,7 +15,7 @@ app.use(cookieParser())
 app.use(cors({
   origin: [
   'http://localhost:5173',
-  'https://esoft-todo-list-production.up.railway.app' 
+  'https://esoft-todo-list-front.up.railway.app' 
 ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
