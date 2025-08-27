02f965657e2f26215d725ee1342490c5bf62e4cd
Kirill <prostoskvasil@gmail.com>
2025-08-28 01:06:28 +0500

    cors changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,7 +13,10 @@ const PORT = process.env.PORT || 3000
 app.use(cookieParser())
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+  'http://localhost:5173',
+  'https://esoft-todo-list-production.up.railway.app' 
+],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
